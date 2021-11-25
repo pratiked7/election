4f7f15092104977b099146f95b17b52262c568e1
pratiked7 <pratiked7@gmail.com>
2021-11-25 10:00:34 +0530

    first

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.2;
+
+contract Election {
+    //store candidate
+    //read candidate
+    string public candidate;
+    //constructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
