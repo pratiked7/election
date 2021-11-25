ac67aef8b126f29345a3e43aa1aec0d4aa280822
pratiked7 <pratiked7@gmail.com>
2021-11-25 10:24:08 +0530

    constract updated

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
 pragma solidity >=0.4.2;
 
 contract Election {
-    //store candidate
-    //read candidate
-    string public candidate;
+    //model a candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //store candidates
+    //fetch candidate
+    mapping(uint => Candidate) public candidates;
+    //store candidates count
+    uint public candidatesCount;
+    
     //constructor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
